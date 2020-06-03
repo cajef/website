d0b35aa6c8c737daa167c783e2c3e2aa844f048e
Walter Melo <walmello24@gmail.com>
2020-06-03 12:54:13 -0300

    forms modal sending email

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+function sendEmail(id, subject) {
+  let name = document.querySelector(id + " [name=name]").value
+  let email = document.querySelector(id + " [name=email]").value
+  let message = document.querySelector(id + " [name=message]").value
+  let phone = ""
+  try {phone = document.querySelector(id + " [name=phone]").value
+  }catch{}
+
+  console.log(
+    name,email,message,phone, subject
+  )
+
+  let text = ""
+
+  switch (subject) {
+    case 'Contato':      
+      text = /*html*/`
+        <h1>${subject}</h1>
+        <h2>Dados de contato</h2>
+        <dl>
+          <dt>Nome: </dt><dd>${name}</dd>
+          <dt>Email: </dt><dd>${email}</dd>
+        </dl>
+        <p>
+          ${message}  
+        </p>
+      `
+      break;
+
+    case 'Voluntário':      
+      text = /*html*/`
+        <h1>${subject}</h1>
+        <h2>Dados de contato</h2>
+        <dl>
+          <dt>Nome: </dt><dd>${name}</dd>
+          <dt>Email: </dt><dd>${email}</dd>
+          <dt>Telefone: </dt><dd>${email}</dd>
+        </dl>
+        <p>Como posso ajudar?</p>
+        <p>
+          ${message}  
+        </p>
+      `
+
+    case 'Doação':      
+      text = /*html*/`
+      <h1>${subject}</h1>
+      <h2>Dados de contato</h2>
+      <dl>
+        <dt>Nome: </dt><dd>${name}</dd>
+        <dt>Email: </dt><dd>${email}</dd>
+        <dt>Telefone: </dt><dd>${email}</dd>
+      </dl>
+      <p>O que posso doar?</p>
+      <p>
+        ${message}  
+      </p>
+      `
+      break;
+
+
+    default:
+      break;
+  }
+
+  Email.send({
+  Host: "smtp.gmail.com",
+  Username : 'ong.cajef@gmail.com',
+  Password : 'cajef2020',
+  To : 'ong.cajef@gmail.com',
+  From : email,
+  Subject : subject,
+  Body : text,
+  }).then(
+    message => window.location.reload()
+  );
+}
